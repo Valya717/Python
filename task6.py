e3290a9f55be1287eedcf4f3112bb418c88c76c7
Valentina Igorevna <valyak1988@mail.ru>
2023-06-16 15:47:09 +0500

    Решение задачи 6-уточненное

--- task6.py
+++ task6.py
@@ -6,6 +6,4 @@
 # 123456 -> no
 
 number= int(input ('Введите номер билета:'))
-a = (number % 10 + (number // 10) % 10 + (number // 100) % 10)
-b = ((number // 1000) % 10 + (number // 10000) % 10 + (number // 100000))
 print(f"yes!!!" if (number % 10 + (number // 10) % 10 + (number // 100) % 10) == ((number // 1000) % 10 + (number // 10000) % 10 + (number // 100000)) else f"no")
