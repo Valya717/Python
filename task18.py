73f0944e8df2eb9c44b200349748bb7b0f37249e
Valentina Igorevna <valyak1988@mail.ru>
2023-06-29 15:42:32 +0500

    задача 18

--- task18.py
+++ task18.py
@@ -11,53 +11,50 @@
 n = int(input('Введите колличество элементов: '))
 a = [int(input('Введите числа: ')) for i in range(n)]
 x = int(input('Введите искомое число: '))
-
-if a[0]>=x:
-    if a[0] == x:
-        print(f'Число {a[0]} равно {x}')
-    else:
-        max_for_x = a[0]
-        min = x
-        min_for_x = 0
-        for i in range(n-1):
-            if a[i+1] == x:
-                print(f'Число {a[i+1]} равно {x}')
-            else:
-                if a[i+1]>x and a[i+1]< max_for_x:
-                    max_for_x = a[i+1]
-                else:
-                    if a[i+1]<x and a[i+1] < min:
-                        min = a[i+1]
-                    elif a[i+1]<x and a[i+1] > min:
-                        min_for_x = a[i+1] 
-        if ((x - min) == ( max_for_x -x )):
-            print(f'Числа{min_for_x, max_for_x} ближе всех к {x}')
-        else:
-            print(f'Число {min_for_x} ближе всех к {x}' if (min_for_x != 0) and (x- min_for_x) < ( max_for_x -x ) else f'Число {max_for_x} ближе всех к {x}')
-
+flag =True
+for i in range (n):
+    if a[i]==x:
+        flag = False
+if flag == False:
+    print('Число есть в массиве')
 else:
-    min_for_x = a[0]
-    max = 0
-    max_for_x = x
-    for i in range(n-1):
-        if a[i+1] == x:
-            print(f'Число {a[i+1]} равно {x}')
+    if a[0]>x:
+    
+        max_to_x = a[0]
+        min_to_x = x
+        for i in range(n-1):
+            if a[i+1]>x and a[i+1]< max_to_x:
+                max_to_x = a[i+1]
+            else:
+                if a[i+1]<x and  min_to_x==x:
+                    min_to_x = a[i+1]
+                elif a[i+1]<x and a[i+1]>min_to_x:
+                    min_to_x = a[i+1]
+        if ((x - min_to_x) == ( max_to_x -x )):
+            print(f'Числа{min_to_x, max_to_x} ближе всех к {x}')
         else:
-            if a[i+1]<x and a[i+1]> min_for_x:
-                min_for_x = a[i+1]
-            elif a[i+1] > x and max_for_x == x:
-                max = max_for_x = a[i+1]
+            print(f'Число {min_to_x} ближе всех к {x}' if (min_to_x != x) and (x- min_to_x) < ( max_to_x -x ) else f'Число {max_to_x} ближе всех к {x}')
+
+    else:
+        min_to_x = a[0]
+        max = 0
+        max_to_x = x
+        for i in range(n-1):
+        
+            if a[i+1]<x and a[i+1]> min_to_x:
+                min_to_x = a[i+1]
+            elif a[i+1] > x and max_to_x == x:
+                max = max_to_x = a[i+1]
             elif a[i+1]>x and a[i+1]> max:
                 max = a[i+1]
-            elif a[i+1]>x and a[i+1]<max_for_x:
-                max_for_x = a[i+1]
-    print(min_for_x)
-    print(max)
-    print(max_for_x)
-    if ((max_for_x - x) == (x - min_for_x)):
-        print(f'Числа{min_for_x, max_for_x} ближе всех к {x}')
-    else:
-        print(f'Число {max_for_x} ближе всех к {x}' if (max_for_x !=0) and (max_for_x - x) < (x - min_for_x) else f'Число {min_for_x} ближе всех к {x}')
+            elif a[i+1]>x and a[i+1]<max_to_x:
+                max_to_x = a[i+1]
+        if min_to_x>max:
+            print(f'Число {min_to_x} ближе всех к {x}')
+        elif ((max_to_x - x) == (x - min_to_x)):
+            print(f'Числа{min_to_x, max_to_x} ближе всех к {x}')
+        else:
+            print(f'Число {max_to_x} ближе всех к {x}' if (max_to_x !=0) and (max_to_x - x) < (x - min_to_x) else f'Число {min_to_x} ближе всех к {x}')
 
 
 
